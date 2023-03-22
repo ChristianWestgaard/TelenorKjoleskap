eae9d3250a5b8397d099a903d9b2d88f3809a439
Christian Westgaard <westgaard04@gmail.com>
2023-03-22 15:02:32 +0100

    func

--- app.js
+++ app.js
@@ -20,3 +20,10 @@ initializeApp(firebaseConfig);
 const db = getFirestore()
 const database = collection(db, 'Users')
 console.log(database);
+
+function vipps15() {
+    alert("Vipps 15KR til 419 21 054")
+}
+function vipps20() {
+    alert("Vipps 20KR til 419 21 054")
+}
