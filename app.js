9ca97fe493e75dad6d6f1c3345fc3d93d3fe6362
Christian Westgaard <westgaard04@gmail.com>
2023-03-22 13:45:55 +0100

    NODE

---
+++ app.js
@@ -0,0 +1,22 @@
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, getDocs, addDoc, deleteDoc, doc } = require('firebase/firestore');
+
+const bodyParser = require('body-parser')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwg-aiXLJqiK0tiviv3dKhhdOH6nvUN-Q",
+    authDomain: "telenokoleskap.firebaseapp.com",
+    projectId: "telenokoleskap",
+    storageBucket: "telenokoleskap.appspot.com",
+    messagingSenderId: "883726078953",
+    appId: "1:883726078953:web:9113fe99047675d471b178",
+    measurementId: "G-49Y0GLE44M"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+const db = getFirestore()
+const database = collection(db, 'Users')
+console.log(database);
